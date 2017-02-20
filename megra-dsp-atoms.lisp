c4c41b845c26666a4cb0be7cfcb21b33a1d73f7c
nik <nik@parkellipsen.de>
2017-02-20 23:05:41 +0100

    two versions of the ambisonics panner ...

--- megra-dsp-atoms.lisp
+++ megra-dsp-atoms.lisp
@@ -1,5 +1,41 @@
 (in-package :scratch)
 
+;; tito's optimized version
+(define-vug pan-ambi-3rd-sn3d-tito (in azi ele)
+  "3rd order ambisonics encoder (no distance coding), ACN, SN3D"
+  (with-samples ((ux (* (cos azi) (cos ele)))
+                 (uy (* (sin azi) (cos ele)))
+                 (uz (sin ele))
+		 (ux2 (* ux ux))
+		 (uy2 (* uy uy))
+		 (uz2 (* uz uz)))
+    (with ((chans (make-frame 16)))
+      (declare (frame chans))
+      (initialize
+       (setf (smp-ref chans 0) (sample 1))
+       (with-follow (ele)
+         (setf (smp-ref chans 2) uz
+               (smp-ref chans 6) (* 0.5 (- (* 3 uz2) 1))
+               (smp-ref chans 12) (* 0.5 uz (- (* 5 uz2) 3))))
+       (with-follow (azi ele)
+         (setf (smp-ref chans 1) uy
+               (smp-ref chans 3) ux
+               (smp-ref chans 4) (* 1.73205080757 ux uy)
+               (smp-ref chans 5) (* 1.73205080757 uy uz)
+               (smp-ref chans 7) (* 1.73205080757 ux uz)
+               (smp-ref chans 8) (* 0.86602540378 (- ux2 uy2))
+               (smp-ref chans 9) (* 0.79056941504 uy (- (* 3 ux2) uy2))
+               (smp-ref chans 10) (* 3.87298334621 uz ux uy)
+               (smp-ref chans 11) (* 0.61237243569 uy (- (* 5 uz2) 1))
+               (smp-ref chans 13) (* 0.61237243569 ux (- (* 5 uz2) 1))
+               (smp-ref chans 14) (* 1.9364916731 uz (- ux2 uy2))
+               (smp-ref chans 15) (* 0.79056941504 ux (- ux2 (* 3 uy2))))))
+      (if (< current-channel 16)
+          (* (smp-ref chans current-channel)
+             (let ((current-channel 0)) in))
+          +sample-zero+))))
+
+;; my original version
 (define-vug pan-ambi-3rd-sn3d (in azi ele)
   "3rd order ambisonics encoder (no distance coding), ACN, SN3D"
   (with-samples ((ux (* (cos azi) (cos ele)))
@@ -20,7 +56,7 @@
 	  ((= current-channel 9) (* 0.79056941504 uy (- (* 3 ux2) uy2) in))
 	  ((= current-channel 10) (* 3.87298334621  uz ux uy in))
 	  ((= current-channel 11) (* 0.61237243569  uy (- (* 5 uz2) 1) in))
-	  ((= current-channel 12) (* 0.5 uz (- (* 5 uz2) 3) in))
+	  ((= current-channel 12) (* 0.5 uz (- (* 5 uz2) 3) in))				       
 	  ((= current-channel 13) (* 0.61237243569 ux (- (* 5 uz2) 1) in))
 	  ((= current-channel 14) (* 1.9364916731 uz (- ux2 uy2) in))
 	  ((= current-channel 15) (* 0.79056941504 ux (- ux2 (* 3 uy2)) in))
