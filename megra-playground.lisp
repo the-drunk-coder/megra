e2d8c81ac2790b5558e56e920e1ae31b49fc91bb
nik <nik@parkellipsen.de>
2017-02-26 14:08:00 +0100

    split startup file from playground

--- megra-playground.lisp
+++ megra-playground.lisp
@@ -1,39 +1,3 @@
-(require 'cm)
-(in-package :cm)
-
-;; initialize -- seems like it has to be like this ...
-(progn
-  (incudine:set-rt-block-size 128)
-  (incudine:rt-start)
-  (sleep 1)
-  (midi-open-default :direction :input)
-  (midi-open-default :direction :output)
-  (osc-open-default :host "127.0.0.1" :port 3002 :direction :input)
-  ;; send osc msgs to scsynth ...
-  (osc-open-default :host "127.0.0.1" :port 57110 :direction :output)    
-  (setf *out* (cm::new cm::incudine-stream))
-  (setf *rts-out* *out*))
-
-(incudine:rt-stop)
-
-;; then load the megra dsp stuff .. wait until compilation has finished !!
-(compile-file "megra-dsp-atoms")
-
-(load "megra-dsp-atoms")
-
-;; wait !
-(compile-file "megra-dsp")
-
-(load "megra-dsp")
-
-;; now everything should be ready to load the megra package ... 
-(load "megra-package")
-
-(in-package :megra)
-
-(defparameter *default-dsp-backend* 'inc)
-;;(defparameter *default-dsp-backend* 'sc)
-
 ;; define some test graph structures
 (graph 'uno-midi ()
        (node 1 (mid 65 :lvl .8 :dur 200))
---
+++ megra-start.lisp
@@ -0,0 +1,35 @@
+(require 'cm)
+(in-package :cm)
+
+;; initialize -- seems like it has to be like this ...
+(progn
+  (incudine:set-rt-block-size 128)
+  (incudine:rt-start)
+  (sleep 1)
+  (midi-open-default :direction :input)
+  (midi-open-default :direction :output)
+  (osc-open-default :host "127.0.0.1" :port 3002 :direction :input)
+  ;; send osc msgs to scsynth ...
+  (osc-open-default :host "127.0.0.1" :port 57110 :direction :output)    
+  (setf *out* (cm::new cm::incudine-stream))
+  (setf *rts-out* *out*))
+
+(incudine:rt-stop)
+
+;; then load the megra dsp stuff .. wait until compilation has finished !!
+(compile-file "megra-dsp-atoms")
+
+(load "megra-dsp-atoms")
+
+;; wait !
+(compile-file "megra-dsp")
+
+(load "megra-dsp")
+
+;; now everything should be ready to load the megra package ... 
+(load "megra-package")
+
+(in-package :megra)
+
+(defparameter *default-dsp-backend* 'inc)
+;;(defparameter *default-dsp-backend* 'sc)
