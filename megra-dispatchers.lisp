ba298940893eaf5b430c9fed35bc156b56011b4a
nik <nik@parkellipsen.de>
2017-06-13 22:36:12 +0200

    even simpler dispatching, but no step dispatching for now ...

--- megra-dispatchers.lisp
+++ megra-dispatchers.lisp
@@ -26,28 +26,37 @@
 	;;(incudine:at next #'incudine:nrt-funcall #'(lambda () (perform-dispatch d proc next)))))))
 	(incudine:at next #'perform-dispatch proc next)))))
 
-;; manual step-by step dispatching ...
-(defun step-dispatch (proc)
-  (when (and (gethash proc *processor-directory*) (is-active (gethash proc *processor-directory*)) )
-    (handle-events (pull-events (gethash proc *processor-directory*)))
-    (handle-transition (car (pull-transition (gethash proc *processor-directory*))))))
-
 (defun handle-transition (tr)
   (transition-duration tr))	 
 
-;; dummy dispatcher for testing and development
-;;(defclass string-dispatcher (dispatcher) ())
-
-;;(defmethod handle-events ((s string-dispatcher) events &key)
-;;  (fresh-line)
-;;  (princ "the following events should be handled: ")
-;;  (mapc #'(lambda (event)
-;;	    (princ (event-message event))
-;;	    (princ " from ")
-;;	    (princ (event-source event))
-;;          (princ ", ")) events))
-
-;; the main event dispatcher
 (defun handle-events (events)
   (mapc #'handle-event events))
+
+(defmacro dispatch ((&key (sync-to nil) (unique t) (chain nil)) &body proc-body)
+  `(funcall #'(lambda () (let ((event-processors (list ,@proc-body)))		      
+		      (when (and ,unique (not ,chain))
+			(detach (gethash (car (last event-processors)) *processor-directory*) event-processors)) 
+		      (when (not ,chain)
+			(connect event-processors))		      
+		      (if (and ,sync-to (gethash ,sync-to *processor-directory*))
+			  ;; if this processor is synced to another, don't start now ..
+			  ;; dispatching will be started by the processor this one is synced to 
+			  (progn			    
+			    (deactivate (car event-processors) :del nil)
+			    (setf (synced-processors (gethash ,sync-to *processor-directory*))
+				  (append (synced-processors (gethash ,sync-to *processor-directory*))
+					  (list (car event-processors)))))
+			  ;; if the first event-processor is not active yet,
+			  ;; create a dispatcher to dispatch it ... 
+			  (unless (is-active (gethash (car event-processors) *processor-directory*))
+			    (activate (car event-processors))
+			    ;; the step dispatching 
+			    ;; with chain rebuilding for
+			    ;; each dispatch
+			    ;; is pretty inefficient and currently
+			    ;; only intended for debugging purposes.
+			    ;; If it should become a regular feature, i might need to this concept ... 			             
+			    (incudine:at (incudine:now) #'perform-dispatch (car event-processors) (incudine:now) ,repeat)
+			    ;;(incudine:at (incudine:now) #'incudine:nrt-funcall #'(lambda () (perform-dispatch dispatcher (car event-processors) (incudine:now))))
+			    ))))))
 
