fca22faf66aed4e44bb3aa541ad2decddce2cd4c
nik <nik@parkellipsen.de>
2017-09-27 23:44:07 +0200

    added filters

--- megra-event-filters.lisp
+++ megra-event-filters.lisp
@@ -8,4 +8,10 @@
 
 (defun snare-p (event)
   (or (member 'snare (event-tags event))
-      (member 'sn(event-tags event))))
+      (member 'sn (event-tags event))))
+
+(defun ride-p (event)
+  (member 'ride (event-tags event)))
+
+(defun hat-p (event)
+  (member 'hat (event-tags event)))
