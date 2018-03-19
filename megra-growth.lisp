1e058c803d96882e500154be8b6f75ad6905878b
nik <nik@parkellipsen.de>
2018-03-19 23:21:24 +0100

    slightly more stable pruning

--- megra-growth.lisp
+++ megra-growth.lisp
@@ -35,21 +35,44 @@
     (insert-edge (source-graph g) (edge new-id dest-id :dur new-dur :prob 50))
     (insert-edge (source-graph g) (edge dest-id new-id :dur new-dur :prob 20))))
 
-(defmethod prune-graph ((g graph-event-processor) &key)
-  (let* ((path (traced-path g)) ;; get the trace ...
-	 (prune-path-idx (+ 1 (random (- (length path) 2))))
-	 (prune-idx (nth prune-path-idx path))
-	 (source-id (nth (- prune-path-idx 1) path))
-	 (dest-id (nth (+ prune-path-idx 1) path))
-	 (old-edge (get-edge (source-graph g) (list source-id) prune-idx))
-	 (new-dur (transition-duration (car (edge-content old-edge))))
-	 (new-prob (edge-probability old-edge)))
-    (insert-edge (source-graph g) (edge source-id dest-id :dur new-dur :prob new-prob))
-    (remove-node (source-graph g) prune-idx)))
+(defun remove-all (items seq)
+  (let ((first-removed (remove (car items) seq)))
+    (if (and (cdr items) first-removed)
+        (remove-all (cdr items) first-removed)
+	first-removed)))
 
+(defmethod prune-graph ((g graph-event-processor) &key exclude durs)
+  (let* ((path (traced-path g)) ;; get the trace ...
+	 (reduced-path (remove-all exclude path)))
+    (format t "~D~%" path)
+    (format t "~D~%" reduced-path)
+    (when (> (length reduced-path) 3)
+      (let* ((prune-path-idx (+ 1 (random (- (length reduced-path) 2))))
+	     (prune-idx (nth prune-path-idx reduced-path))
+	     (source-id (nth (- prune-path-idx 1) reduced-path))
+	     (dest-id (nth (+ prune-path-idx 1) reduced-path))
+	     (old-edge (get-edge (source-graph g) (list source-id) prune-idx)))
+	(if old-edge
+	    (let ((new-dur (transition-duration (car (edge-content old-edge))))
+		  (new-prob (edge-probability old-edge)))
+	      (insert-edge (source-graph g)
+			   (edge source-id dest-id :dur new-dur :prob new-prob)))
+	    (let ((new-dur (if durs
+			       (nth (random (length durs)) durs)
+			       (transition-duration
+				(car (edge-content
+				      (get-edge (source-graph g)
+						(list (car path))
+						(cadr path))))))))
+	      (insert-edge (source-graph g) (edge source-id dest-id :dur new-dur :prob 0))
+	      (rebalance-edges (source-graph g))
+	      ))
+	;; finally, remove the node to be pruned !
+	(remove-node (source-graph g) prune-idx)))))
+    
 (defun grow (graph-id &key (variance 0) durs)
   (grow-graph (gethash graph-id *processor-directory*) :var variance :durs durs))
 
-(defun prune (graph-id)
-  (prune-graph (gethash graph-id *processor-directory*)))
+(defun prune (graph-id &key exclude durs)
+  (prune-graph (gethash graph-id *processor-directory*) :exclude exclude :durs durs))
 
