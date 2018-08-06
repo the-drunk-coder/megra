b15e6464aec7be261a77bf7d9e3d5e7fd7fc744e
nik <nik@parkellipsen.de>
2018-08-06 23:12:58 +0200

    automatic predicates for all events

--- megra-event-filters.lisp
+++ megra-event-filters.lisp
@@ -3,16 +3,9 @@
 ;; some default event filters
 (defmethod all-p ((e event) &key) t)
 
-(defmethod transition-p ((e event) &key)
-  (typep e 'transition))
-
 (defun snare-p (event)
   (or (member 'snare (event-tags event))
       (member 'sn (event-tags event))))
-
-(defun bd-p (event)
-  (or (member 'bassdrum (event-tags event))
-      (member 'bd (event-tags event))))
 
 (defun ride-p (event)
   (member 'ride (event-tags event)))
