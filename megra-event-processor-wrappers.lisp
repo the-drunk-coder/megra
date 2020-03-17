7324e79511288e52ac497f904a9fcf27aee55e37
nik <nik@parkellipsen.de>
2020-03-17 22:31:37 +0100

    bring back prob ctrl (still dropping out)

--- megra-event-processor-wrappers.lisp
+++ megra-event-processor-wrappers.lisp
@@ -57,9 +57,11 @@
                                       :method method :rnd rnd :higher-order higher-order))
 
 (defmethod post-processing ((w event-processor-wrapper) &key))
+
 ;;;;;;;;;;;;;;;; GENERIC Population Control ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; population here refers to the number of nodes in a graph ...
+;; population here refers to the number of symbols in a generator's alphabet ...
+;; implementations of this can be found in 'megra-lifemodel' and 'megra-probctrl'
 
 (defclass generic-population-control ()
   ((variance :accessor population-control-var :initarg :variance)
@@ -69,15 +71,7 @@
    (hoe-max :accessor population-control-higher-order-max-order :initarg :hoe-max)
    (exclude :accessor population-control-exclude :initarg :exclude)))
 
-;;;;;;;;;;;;;;;; Simple Probablistic Population Control ;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; grow and prune the graph with a certain probability
-
-(defclass probability-population-control (event-processor-wrapper
-					  generic-population-control)
-  ((pgrowth :accessor population-control-pgrowth :initarg :pgrowth)
-   (pprune :accessor population-control-pprune :initarg :pprune)))
-
+;; helper function for shorthands ...
 (defun find-keyword-val (keyword seq &key default)
   (if (and
        (member keyword seq)
@@ -88,196 +82,8 @@
 	val)
       default))
 
-(defun probctrl (pgrowth pprune &rest rest)
-  (let ((method (find-keyword-val :method rest :default 'triloop))
-	(variance (find-keyword-val :var rest :default 0.2))
-	(durs (find-keyword-val :durs rest :default nil))
-	(hoe-max (find-keyword-val :hoe-max rest :default 4))
-	(hoe (find-keyword-val :hoe rest :default 4))
-	(exclude (find-keyword-val :exclude rest :default nil))
-	(wrapped-processor (if (typep (last rest) 'symbol)
-			       (gethash (last rest) *processor-directory*)
-			       (car (last rest)))))
-    (make-instance 'probability-population-control
-		   :wrapped-processor wrapped-processor		   
-		   :name (intern (format nil "~D-probctrl" (name wrapped-processor)))
-		   :variance variance
-		   :pgrowth pgrowth
-		   :pprune pprune
-		   :method method
-		   :durs durs
-		   :phoe hoe
-		   :hoe-max hoe-max
-		   :exclude exclude)))
-
-(defmethod post-processing ((g probability-population-control) &key)
-  (when (< (random 100) (population-control-pgrowth g))
-    (let ((order (if (< (random 100)
-			(population-control-higher-order-probability g))
-		     (+ 2 (random
-			   (- (population-control-higher-order-max-order g) 2)))
-		     nil)))
-      (grow (wrapper-wrapped-processor g)
-	    :var (population-control-var g)
-	    :durs (population-control-durs g)
-	    :method (population-control-method g)
-	    :higher-order order)))
-  (when (< (random 100) (population-control-pprune g))
-    (prune-graph
-     (wrapper-wrapped-processor g)       
-     :exclude (population-control-exclude g))))
-
-
-;;;;;;;;;;;;;;;; Simple Artifical Life Model ;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; growing an eventprocessor using a resource model ... cold also be named
-;; free energy model or something like that ... 
-
-(defclass lifemodel-control (event-processor-wrapper generic-population-control)
-  ((growth-cycle :accessor lmc-growth-cycle :initarg :growth-cycle)
-   (lifecycle-count :accessor lmc-lifecycle-count :initform 0)
-   (apoptosis :accessor lmc-apoptosis :initarg :apoptosis :initform t)
-   (node-lifespan :accessor lmc-node-lifespan :initarg :node-lifespan :initform *average-node-lifespan*)
-   (node-lifespan-var :accessor lmc-node-lifespan-var :initarg :node-lifespan-var :initform *node-lifespan-variance*)
-   (autophagia :accessor lmc-autophagia :initarg :autophagia :initform t)
-   (local-resources :accessor lmc-local-resources :initarg :local-resources :initform *default-local-resources*)
-   (local-cost :accessor lmc-local-cost :initarg :local-cost-modifier :initform *growth-cost*)
-   (local-apoptosis-regain :accessor lmc-local-apoptosis-regain :initarg :apoptosis-regain :initform *apoptosis-regain*)
-   (local-autophagia-regain :accessor lmc-local-autophagia-regain :initarg :autophagia-regain :initform *autophage-regain*)))
-
-(defmethod post-processing ((l lifemodel-control) &key)  
-  (incf (lmc-lifecycle-count l))  
-  ;; growth point reached
-  (let* ((src (graph-id (source-graph (wrapper-wrapped-processor l))))
-	 (cur-node-id (current-node (wrapper-wrapped-processor l)))
-	 (cur-node (gethash cur-node-id (graph-nodes (source-graph (wrapper-wrapped-processor l)))))
-	 (eaten-node 0)) ;; node "eaten" by autophagia ...	     
-    ;; growth or no growth ?
-    (when (>= (lmc-lifecycle-count l) (lmc-growth-cycle l))
-      (setf (lmc-lifecycle-count l) 0) ;; reset growth cycle
-      (if (> (+ (lmc-local-resources l) *global-resources*) (lmc-local-cost l))
-	  ;; first case: enough resoures available
-	  (let ((order (if (< (random 100)
-			      (population-control-higher-order-probability l))
-			   (+ 2 (random
-				 (- (population-control-higher-order-max-order l) 2)))
-			   nil)))
-	    ;; grow graph 
-	    (grow (wrapper-wrapped-processor l)
-		  :var (population-control-var l)
-		  :durs (population-control-durs l)
-		  :method (population-control-method l)
-		  :higher-order order)	    
-	    ;; decrease resources
-	    (if (>= (lmc-local-resources l) (lmc-local-cost l))
-		(setf (lmc-local-resources l) (- (lmc-local-resources l)
-						 (lmc-local-cost l)))
-		(if (>= *global-resources* (lmc-local-cost l))
-		    (setf *global-resources* (- *global-resources* (lmc-local-cost l)))
-		    ;; otherwise, split ...
-		    (let ((tmp-cost (lmc-local-cost l)))
-		      (setf tmp-cost (- tmp-cost (lmc-local-resources l)))
-		      (setf (lmc-local-resources l) 0.0)
-		      (setf *global-resources* (- *global-resources* tmp-cost)))))
-	    (incudine::msg info "GROW at ~D - local: ~D global: ~D"
-			   src
-			   (lmc-local-resources l)
-			   *global-resources*))
-	  ;; else: autophagia if specified ...
-	  (when (and (lmc-autophagia l)
-		     (> (graph-size (source-graph (wrapper-wrapped-processor l))) 1))	    
-	    ;; send prune/shrink
-	    (let ((rnd-node (random-node-id (source-graph (wrapper-wrapped-processor l)) nil)))
-	      (prune-graph (wrapper-wrapped-processor l) :node-id cur-node-id)
-	      (setf eaten-node rnd-node))	    
-	    ;; add regain to local	    
-	    (setf (lmc-local-resources l)
-		  (+ (lmc-local-resources l)
-		     (lmc-local-autophagia-regain l)))	    
-	    (incudine::msg info "AUTO at ~D - local: ~D global: ~D - ~D is starving!"
-			   src
-			   (lmc-local-resources l)
-			   *global-resources*
-			   src))))
-    ;; handle apoptosis:
-    ;; check if current node is old enough (regarding eventual variance)
-    ;; delete if old
-    ;; add regain ... 
-    (when (and
-	   (lmc-apoptosis l)
-	   (or (and *dont-let-die* (> (graph-size (source-graph (wrapper-wrapped-processor l))) 1))
-	       (not *dont-let-die*))	       
-	   (> (node-age cur-node)
-	      (add-var (lmc-node-lifespan l)
-		       (lmc-node-lifespan-var l))))
-      (unless (eql cur-node-id eaten-node)
-	(prune-graph (wrapper-wrapped-processor l) :node-id cur-node-id)    
-	(setf (lmc-local-resources l)
-	      (+ (lmc-local-resources l)
-		 (lmc-local-apoptosis-regain l))))      
-      (incudine::msg info
-		     "APOP at ~D - local: ~D global: ~D - node ~D your time has come !"
-		     src		     		     
-		     (lmc-local-resources l)
-		     *global-resources*
-		     cur-node-id))))
-
-(defun add-var (orig var)
-  (floor (+ orig (* (* (- 20000 (random 40000)) var)
-	            (/ orig 20000)))))
-
-;; lifemodel works more in minimalistic contexts rather than algorave,
-;; i suppose ...
-(defun inner-lifemodel (growth-cycle lifespan rest)
-  (let ((method (find-keyword-val :method rest :default 'triloop))
-	(variance (find-keyword-val :var rest :default 0.2))
-	(autophagia (find-keyword-val :autophagia rest :default t))
-	(apoptosis (find-keyword-val :apoptosis rest :default t))        
-	(durs (find-keyword-val :durs rest :default nil))
-	(hoe-max (find-keyword-val :hoe-max rest :default 4))
-	(hoe (find-keyword-val :hoe rest :default 4))
-	(exclude (find-keyword-val :exclude rest :default nil))
-	(wrapped-processor (if (typep (last rest) 'symbol)
-			       (gethash (last rest) *processor-directory*)
-			       (car (last rest)))))
-    (make-instance 'lifemodel-control
-		   :name (intern (format nil "~D-lifemodel" (name wrapped-processor)))
-		   :wrapped-processor wrapped-processor
-		   :growth-cycle growth-cycle		 
-		   :variance variance		 
-		   :method method
-		   :durs durs
-		   :phoe hoe
-		   :node-lifespan lifespan
-		   :hoe-max hoe-max
-		   :exclude exclude
-		   :autophagia autophagia
-		   :apoptosis apoptosis)))
-
-(defun lifemodel (growth-cycle lifespan &rest rest)
-  (inner-lifemodel growth-cycle lifespan rest))
-
-(defun lm (growth-cycle lifespan &rest rest)
-  (inner-lifemodel growth-cycle lifespan rest))
-
-(defun life (growth-cycle lifespan var method &optional proc)
-  (if proc
-      (if (typep proc 'function)
-          (lambda (pproc) (life growth-cycle lifespan var method (funcall proc pproc)))
-          (make-instance 'lifemodel-control
-		         :name (intern (format nil "~D-lifemodel" (name proc)))
-		         :wrapped-processor proc
-		         :growth-cycle growth-cycle		 
-		         :variance var		 
-		         :method method
-		         :durs nil
-		         :phoe 4
-		         :node-lifespan lifespan
-		         :hoe-max 4
-		         :exclude nil
-		         :autophagia t
-		         :apoptosis nil))
-      (lambda (pproc) (life growth-cycle lifespan var method pproc))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;; SOME RANDOM PROCESSOR WRAPPERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;         NEEDS CLEAN-UP          ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; count
 (defclass count-wrapper (event-processor-wrapper)
---
+++ megra-probctrl.lisp
@@ -0,0 +1,63 @@
+(in-package :megra)
+
+;;;;;;;;;;;;;;;; Simple Probablistic Population Control ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; grow and prune the graph with a certain probability
+
+(defclass probability-population-control (event-processor-wrapper
+					  generic-population-control)
+  ((pgrowth :accessor population-control-pgrowth :initarg :pgrowth)
+   (pprune :accessor population-control-pprune :initarg :pprune)))
+
+(defmethod post-processing ((g probability-population-control) &key)
+  (when (< (random 100) (population-control-pgrowth g))
+    (grow (wrapper-wrapped-processor g)
+	  :var (population-control-var g)
+	  :durs (population-control-durs g)
+	  :method (population-control-method g)
+	  :higher-order (if (< (random 100)
+			       (population-control-higher-order-probability g))
+		            (+ 2 (random
+			          (- (population-control-higher-order-max-order g) 2)))
+		            0)))
+  (when (< (random 100) (population-control-pprune g))
+    (prune (wrapper-wrapped-processor g) :exclude (population-control-exclude g))))
+
+(defun probctrl (pgrowth pprune &rest rest)
+  (let ((method (find-keyword-val :method rest :default 'triloop))
+	(variance (find-keyword-val :var rest :default 0.2))
+	(durs (find-keyword-val :durs rest :default nil))
+	(hoe-max (find-keyword-val :hoe-max rest :default 4))
+	(hoe (find-keyword-val :hoe rest :default 4))
+	(exclude (find-keyword-val :exclude rest :default nil))
+	(wrapped-processor (if (typep (last rest) 'symbol)
+			       (gethash (last rest) *processor-directory*)
+			       (car (last rest)))))
+    (make-instance 'probability-population-control
+		   :wrapped-processor wrapped-processor		   
+		   :name (intern (format nil "~D-probctrl" (name wrapped-processor)))
+		   :variance variance
+		   :pgrowth pgrowth
+		   :pprune pprune
+		   :method method
+		   :durs durs
+		   :phoe hoe
+		   :hoe-max hoe-max
+		   :exclude exclude)))
+
+(defun pctrl (pgrowth pprune var method &optional proc)
+  (if proc
+      (if (typep proc 'function)
+          (lambda (pproc) (pctrl pgrowth pprune var method (funcall proc pproc)))
+          (make-instance 'probability-population-control
+		         :wrapped-processor proc	   
+		         :name (intern (format nil "~D-probctrl" (name proc)))
+		         :variance var
+		         :pgrowth pgrowth
+		         :pprune pprune
+		         :method method
+		         :durs nil
+		         :phoe 4
+		         :hoe-max 2
+		         :exclude '()))
+      (lambda (pproc) (pctrl pgrowth pprune var method pproc))))
