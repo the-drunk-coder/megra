f723f270dbf29f580a22ae793119eb734deffdae
nik <nik@parkellipsen.de>
2020-03-18 19:39:51 +0100

    remove old type handling from deepcopy

--- megra-deepcopy.lisp
+++ megra-deepcopy.lisp
@@ -173,10 +173,7 @@
 		   'string
 		   (symbol-name proc-name)
 		   "-"
-		   (symbol-name (gensym)))))
-    ;; this is sooo legacy ..
-    (when (typep (wrapper-wrapped-processor clone) 'graph-event-processor)
-      (update-graph-name (source-graph (wrapper-wrapped-processor clone)) (name (wrapper-wrapped-processor clone))))    
+		   (symbol-name (gensym)))))    
     ;; store reference in global processor directory
     (setf (gethash (name (wrapper-wrapped-processor clone)) *processor-directory*)
 	  (wrapper-wrapped-processor clone))
