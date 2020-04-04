8601d7f02dcd2a68587dfefcd3e47f23f8bc5263
nik <nik@parkellipsen.de>
2020-04-04 15:15:30 +0200

    fix to-svg

--- megra-generator.lisp
+++ megra-generator.lisp
@@ -189,11 +189,11 @@
                                   :default-dur default-dur
                                   :successor next)))))  
 
-(defun to-svg (graph-or-id &key (renderer 'dot))
+(defun to-svg (graph-or-id &key (renderer 'vom::dot))
   (let* ((g (if (typep graph-or-id 'symbol)
 	       (gethash graph-or-id *processor-directory*)
 	       graph-or-id))
          (ig (inner-generator g)))
     (if (typep ig 'vom::adj-list-pfa)
         (vom::adj-list-pfa->svg ig (symbol-name (name g)) :renderer renderer)
-        (vom::graph->svg ig (symbol-name (name g))))))
+        (vom::graph->svg ig (symbol-name (name g)):renderer renderer))))
