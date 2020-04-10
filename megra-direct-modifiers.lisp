8de9145b4ce4228b7632240e0602a5ca639c3660
nik <nik@parkellipsen.de>
2020-04-10 18:10:27 +0200

    also as external commands

--- megra-direct-modifiers.lisp
+++ megra-direct-modifiers.lisp
@@ -7,12 +7,12 @@
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (skip num (funcall proc nproc)))
-          (progn
+          (let ((iproc (if (symbolp proc) (gethash proc *processor-directory*) proc)))
             (loop for a from 0 to (- num 1)
                   do (progn                              
-                       (pull-events proc :skip-successor t)
-                       (pull-transition proc :skip-successor t)))
-            proc))
+                       (pull-events iproc :skip-successor t)
+                       (pull-transition iproc :skip-successor t)))
+            iproc))
       (lambda (nproc) (skip num nproc))))
 
 (defun inner-grown (n var rest proc)
@@ -44,9 +44,8 @@
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (haste num mod (funcall proc nproc)))
-          (progn (loop for a from 0 to (- num 1)
-                       do (push-tmod proc mod))
-                 proc))
+          (let ((iproc (if (symbolp proc) (gethash proc *processor-directory*) proc))) 
+            (loop for a from 0 to (- num 1) do (push-tmod iproc mod)) iproc))
       (lambda (nproc) (haste num mod nproc))))
 
 ;; relax 4 0.5 - apply tempo mod for the next n times (only on base proc)
@@ -54,9 +53,8 @@
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (relax num mod (funcall proc nproc)))
-          (progn (loop for a from 0 to (- num 1)
-                       do (push-tmod proc (coerce (/ 1.0 mod) 'float)))
-                 proc))
+          (let ((iproc (if (symbolp proc) (gethash proc *processor-directory*) proc)))            
+            (loop for a from 0 to (- num 1) do (push-tmod iproc (coerce (/ 1.0 mod) 'float))) iproc))
       (lambda (nproc) (relax num mod nproc))))
 
 ;; rew 3 - rewind (set to state n back in traced path)
@@ -64,53 +62,53 @@
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (rew num (funcall proc nproc)))
-          (progn            
-            (if (typep (inner-generator proc) 'vom::adj-list-pfa) 
+          (let ((iproc (if (symbolp proc) (gethash proc *processor-directory*) proc)))            
+            (if (typep (inner-generator iproc) 'vom::adj-list-pfa) 
                 (progn
-                  (setf (vom::current-state (inner-generator proc))
-                        (list (nth (- (vom::history-length (inner-generator proc)) (+ num 1)) (vom::history (inner-generator proc)))))
-                  (setf (vom::history (inner-generator proc))
-                        (append (vom::history (inner-generator proc)) (vom::current-state (inner-generator proc)))))
+                  (setf (vom::current-state (inner-generator iproc))
+                        (list (nth (- (vom::history-length (inner-generator iproc)) (+ num 1)) (vom::history (inner-generator iproc)))))
+                  (setf (vom::history (inner-generator iproc))
+                        (append (vom::history (inner-generator iproc)) (vom::current-state (inner-generator iproc)))))
                 (progn
-                  (setf (vom::current-node (inner-generator proc))
-                        (nth (- (vom::history-length (inner-generator proc)) (+ num 1)) (vom::history (inner-generator proc))))
-                  (setf (vom::history (inner-generator proc))
-                        (append (vom::history (inner-generator proc)) (list (vom::current-node (inner-generator proc)))))))                        
-            proc))
+                  (setf (vom::current-node (inner-generator iproc))
+                        (nth (- (vom::history-length (inner-generator iproc)) (+ num 1)) (vom::history (inner-generator iproc))))
+                  (setf (vom::history (inner-generator iproc))
+                        (append (vom::history (inner-generator iproc)) (list (vom::current-node (inner-generator iproc)))))))                        
+            iproc))
       (lambda (nproc) (rew num nproc))))
 
 (defun rep (prob max &optional proc)  
   (if proc
       (if (typep proc 'function)
-          (lambda (&optional nproc) (rep prob max (funcall proc nproc)))         
-          (progn
-            (loop for sym in (vom::alphabet (inner-generator proc))
-                  do (let ((next (if (cadr (member sym (vom::alphabet (inner-generator proc))))
-                                     (cadr (member sym (vom::alphabet (inner-generator proc))))
-                                     (car (vom::alphabet (inner-generator proc))))))
-                       (when (and (not (vom::has-transition (inner-generator proc) (list sym) sym))
+          (lambda (&optional nproc) (rep prob max (funcall proc nproc)))                   
+          (let ((iproc (if (symbolp proc) (gethash proc *processor-directory*) proc)))
+            (loop for sym in (vom::alphabet (inner-generator iproc))
+                  do (let ((next (if (cadr (member sym (vom::alphabet (inner-generator iproc))))
+                                     (cadr (member sym (vom::alphabet (inner-generator iproc))))
+                                     (car (vom::alphabet (inner-generator iproc))))))
+                       (when (and (not (vom::has-transition (inner-generator iproc) (list sym) sym))
                                   (< (random 100) prob))
-                         (vom::insert-rule (inner-generator proc) (list (list sym) sym (* prob 0.01)))
+                         (vom::insert-rule (inner-generator iproc) (list (list sym) sym (* prob 0.01)))
                          ;;(format t "RULE ~D~% " (list (list sym) sym (* prob 0.01)))
-                         (vom::insert-rule (inner-generator proc) (list (make-list max :initial-element sym)
-                                                                        next 1.0))
+                         (vom::insert-rule (inner-generator iproc) (list (make-list max :initial-element sym)
+                                                                         next 1.0))
                          ;;(format t "MAX RULE ~D~% " (list (make-list max :initial-element sym) next 1.0))
-                         (vom::rebalance-state (inner-generator proc) (list sym)))))
-            proc))
+                         (vom::rebalance-state (inner-generator iproc) (list sym))))))
+          iproc)
       (lambda (nproc) (rep prob max nproc))))
 
 (defun sharpen (factor &optional proc)  
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (sharpen factor (funcall proc nproc)))         
-          (vom::sharpen-pfa (inner-generator proc) factor))
+          (vom::sharpen-pfa (inner-generator (if (symbolp proc) (gethash proc *processor-directory*) proc)) factor))
       (lambda (nproc) (sharpen factor nproc))))
 
 (defun blur (factor &optional proc)  
   (if proc
       (if (typep proc 'function)
           (lambda (&optional nproc) (blur factor (funcall proc nproc)))         
-          (vom::blur-pfa (inner-generator proc) factor))
+          (vom::blur-pfa (inner-generator (if (symbolp proc) (gethash proc *processor-directory*) proc)) factor))
       (lambda (nproc) (blur factor nproc))))
 
 (defun discourage (factor &optional proc)  
